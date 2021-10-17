3437b60e3294ff3bdc2a7f06d28a3ba00f454535
tim310579 <43834434+tim310579@users.noreply.github.com>
2021-10-17 18:51:40 +0800

    add grid search to find best layers, neurons

--- Case Presentation 1 Data/nn_model.py
+++ Case Presentation 1 Data/nn_model.py
@@ -15,9 +15,19 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation
 from tensorflow.keras.utils import to_categorical
 
-seed = 88
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import RFE
+from sklearn.svm import LinearSVC
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
+import random
+
+seed = 1
 tf.random.set_seed(seed)
 np.random.seed(seed)
+os.environ['PYTHONHASHSEED']=str(seed)
+random.seed(seed)
+tf.compat.v1.set_random_seed(seed)
 
 #os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 #del_col = ['is_Obese', 'text_obese', 'obese', 'obesity']
@@ -42,7 +52,7 @@ def load_data(filename):
         
     df = df.fillna(0)
     Y_df = df['is_Obese']
-    if Y_df.dtype=='int64': Y_df = to_categorical(Y_df, 2)
+    #if Y_df.dtype=='int64': Y_df = to_categorical(Y_df, 2)
     X_df = df.copy()
     X_df = X_df.drop(columns=del_col)
 
@@ -55,7 +65,7 @@ def load_data(filename):
     
 #print(X_train, Y_train)
 
-
+'''
 def report_model(model, X_train, Y_train, X_test, Y_test, X_valid):
 
     train_pred = model.predict(X_train)
@@ -80,27 +90,34 @@ def report_model(model, X_train, Y_train, X_test, Y_test, X_valid):
     pred_df.to_csv("./pred.csv", index=False)
     print(pred_df)
     print(pred_df['Obesity'].value_counts())
+    '''
 
-def train_nn_model(X_train, Y_train, X_test, Y_test, X_valid):
+def train_nn_model(X_train, Y_train, X_test, Y_test, X_valid, layer, neurons):
+    
     model = Sequential()
-
+    #tmp = tuple((23, 555))
+    
+    #model.fit(df_train, train_labels, callbacks=[es_callback])
+    
     #layers = np.random.randint(7, 11)
-    layers = 5
-    for i in range(layers):
+        
+    for i in range(layer):
         #neurons = np.random.randint(300, 500)
-        neurons = 1000
+        #neurons = 500
         model.add(Dense(units=neurons, activation='relu'))
         model.add(Dropout(0.3))
         
-
+    
 
     model.add(Dense(units=2, activation='softmax'))
 
     model.compile(loss='categorical_crossentropy',
                       optimizer='adam', metrics=['accuracy'])
-    # fit model
-    model.fit(X_train, Y_train, batch_size=32, epochs=20)
+    es_callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
 
+    # fit model
+    model.fit(X_train, Y_train, callbacks=[es_callback], batch_size=32, epochs=30)
+    #print(len(history.history['loss']))
     # evaluate the model and output the accuracy
     result_train = model.evaluate(X_train, Y_train)
     result_test = model.evaluate(X_test, Y_test)
@@ -114,71 +131,113 @@ def train_nn_model(X_train, Y_train, X_test, Y_test, X_valid):
     pred_df.to_csv("./pred.csv", index=False)
     print(pred_df)
     print(pred_df['Obesity'].value_counts())
-    return pred_df
-
-def report_nn_model(X_train, Y_train, X_test, Y_test, X_valid):
-    results = pd.DataFrame(columns = ['Filename', 'Obesity'])
-    result = train_nn_model(X_train, Y_train, X_test, Y_test, X_valid)
-    results['Filename'] = result['Filename']
-    results['Obesity'] = result['Obesity']
-    #results['Obesity'] = results['Obesity'].astype('int64')
     
-    cases = 30
-    for i in range(cases):
-        result = train_nn_model(X_train, Y_train, X_test, Y_test, X_valid)
-        #result.to_csv("./preds/pred%d.csv"%i, index=False)
-        print(results)
-        results['Filename'] = result['Filename']
-        results['Obesity'] += result['Obesity']
+    
+    return pred_df, result_test[1]
 
-    #print(results)
-    print(results['Obesity'].value_counts().sort_index())
-
-    results['Obesity']/= cases+1
-    #results['Obesity']+= 0.1
-    results['Obesity'] = results['Obesity'].round().astype('int64')
-    # results['Obesity'] = results['Obesity'].astype('int64')
-
-    print(results)
-    print(results['Obesity'].value_counts().sort_index())
-
-    results.to_csv("./pred.csv", index=False)
+X_train, X_test, Y_train, Y_test = pd.DataFrame(),pd.DataFrame(),pd.DataFrame(),pd.DataFrame()
 
 
-
-if __name__ == '__main__':
-    find_useless_feature()
-
-    X_train, X_test, Y_train, Y_test = pd.DataFrame(),pd.DataFrame(),pd.DataFrame(),pd.DataFrame()
+# valid(submittion)
+X_valid, df_file_name = load_data('valid_data.csv')
 
 
-    # valid(submittion)
-    X_valid, df_file_name = load_data('valid_data.csv')
+# train model
+# use original train, test data to train
+X_train, Y_train = load_data('train_data.csv')
+#X_test, Y_test = load_data('test_data.csv')
 
-
-    # train model
-    # use original train, test data to train
-    X_train, Y_train = load_data('train_data.csv')
-    X_test, Y_test = load_data('test_data.csv')
-
-
-
+#pca = PCA(n_components=400)
+#pca.fit(X_train)
+#X_train = pca.transform(X_train)
+#X_valid = pca.transform(X_valid)
+    
+    
     # use merge(train+test, 428 datas) to train
     #X_merge_data, Y_merge_data = load_data('merge_data.csv')
     #X_train, X_test, Y_train, Y_test = train_test_split(X_merge_data, Y_merge_data, stratify=Y_merge_data, test_size=0.3, random_state=88)
 
     # X_train, Y_train = X_merge_data.copy(), Y_merge_data.copy()
-    # X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, stratify=Y_train, test_size=0.3, random_state=88)
+X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, stratify=Y_train, test_size=0.3, random_state=seed)
 
 
+def experiment(k, layer, neuron):
+    selector = 0
+    selector = SelectKBest(f_classif, k=k)
+    selector.fit(X_train, Y_train)
+    
+    X_train_tmp = selector.transform(X_train)
+    X_test_tmp = selector.transform(X_test)
+    X_valid_tmp = selector.transform(X_valid)
+    
+    Y_train_tmp = to_categorical(Y_train, 2)
+    Y_test_tmp = to_categorical(Y_test, 2)
 
-    model_rf = RandomForestClassifier(
+    result, test_acc = train_nn_model(X_train_tmp, Y_train_tmp, X_test_tmp, Y_test_tmp, X_valid_tmp, layer, neuron)
+
+    return result, test_acc
+
+if __name__ == '__main__':
+    find_useless_feature()
+
+    
+    #K_best = [50,100]
+    #layers = [3,5]
+    #neurons = [100,200]
+
+    K_best = [50,100,150,200,300,400,500,600]
+    layers = [3,5,7,9,11]
+    neurons = [100,200,300,400,500,600]
+
+    best = 0
+    param = ''
+    df_result = pd.DataFrame(columns = ['param', 'test_acc']) 
+    cnt = 0
+
+    #result, test_acc0 = experiment(k=50, layer=3, neuron=100)
+    #result, test_acc1 = experiment(k=50, layer=3, neuron=200)
+    
+    #result, test_acc2 = experiment(k=50, layer=5, neuron=100)
+    #result, test_acc3 = experiment(k=50, layer=5, neuron=200)
+    
+    #result, test_acc4 = experiment(k=100, layer=3, neuron=100)
+    #result, test_acc5 = experiment(k=100, layer=3, neuron=200)
+    #result, test_acc6 = experiment(k=100, layer=5, neuron=100)
+    #result, test_acc7 = experiment(k=100, layer=5, neuron=200)
+
+    #print(test_acc0, test_acc1, test_acc2, test_acc3, test_acc4, test_acc5, test_acc6, test_acc7)
+
+    #gg
+
+    
+    for k in K_best:
+        for layer in layers:
+            for neuron in neurons:
+
+                #like grid search
+                result, test_acc = experiment(k, layer, neuron)
+    
+                
+                df_result.loc[cnt, 'param'] = str(k)+' '+str(layer)+' '+str(neuron)
+                df_result.loc[cnt, 'test_acc'] = test_acc
+                
+                cnt += 1
+                if(best < test_acc):
+                    best = test_acc
+                    param = str(k)+str(layer)+str(neuron)
+        
+    print(df_result)
+    df_result.to_csv('grid_search.csv',index=False)
+    print(best, param)
+    print(cnt)
+    #model_rf = RandomForestClassifier(
          #                  criterion='entropy', max_depth=8, max_features=0.7, max_leaf_nodes=17,
           #                 min_samples_leaf=17, min_samples_split=30,
            #               min_weight_fraction_leaf=0.01, random_state=123
-                            ).fit(X_train, Y_train)
+    #                        ).fit(X_train, Y_train)
     #model = LogisticRegression().fit(X_train, Y_train)
 
-    result = train_nn_model(X_train, Y_train, X_test, Y_test, X_valid)
+    #)
+    #print(Y_test)
     #report_nn_model(X_train, Y_train, X_test, Y_test, X_valid)
     #report_model(model_rf, X_train, Y_train, X_test, Y_test, X_valid)
